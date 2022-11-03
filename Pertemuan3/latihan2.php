e213e031cb4078f36554c76997a7481774d0854c
ABMaulachela <abmaulachela@gmail.com>
2022-11-03 09:26:30 +0800

    File Project Baru 03/11/22

---
+++ Pertemuan3/latihan2.php
@@ -0,0 +1,34 @@
+<?php 
+  // Kondisi atau percabangan
+  // if else
+  // if else if else
+  // ternary
+  // switch
+
+  
+  // if 
+  // $x = 10;
+  // if( $x < 20 ) {
+  //   echo "Benar";
+  // }
+  // echo "Salah";
+
+  // if else
+  // $x = 10;
+  // if( $x < 20 ) {
+  //   echo "Benar";
+  // } else {
+  //   echo "Salah";
+  // }
+  
+  // if else if
+  $x = 10;
+  if( $x < 20 ) {
+    echo "Benar";
+  } else if( $x == 20 ) {
+    echo "Bingo";
+  } else {
+    echo "Salah";
+  }
+  
+?>
